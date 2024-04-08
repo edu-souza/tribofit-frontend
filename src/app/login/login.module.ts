be912557f88d59576bf0dd452e0b61b2ab04880a
Eduardo de Souza <desouzanovesete@gmail.com>
2024-04-07 22:57:27 -0300

    commit inicial

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { TabsPageRoutingModule } from '../tabs/tabs-routing.module';
+import { LoginRoutingModule } from './login-routing.module';
+import { PaginaLoginComponent } from './login/pagina-login.component';
+
+@NgModule({
+  declarations: [PaginaLoginComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoginRoutingModule,
+    TabsPageRoutingModule,
+  ]
+})
+export class LoginModule { }
