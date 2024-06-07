c220072a48514ba2bcc428423aeb4ade687a0d25
Eduardo de Souza <desouzanovesete@gmail.com>
2024-06-07 10:35:44 -0300

    inicio integraão leaflet maps

--- src/app/atividades/detalhe-atividades/detalhe-atividades.component.ts
+++ src/app/atividades/detalhe-atividades/detalhe-atividades.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ViewDidEnter } from '@ionic/angular';
+import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'detalhe-atividades',
   templateUrl: './detalhe-atividades.component.html',
   styleUrls: ['detalhe-atividades.component.css']
 })
-export class DetalheAtividadesComponent {
+export class DetalheAtividadesComponent implements OnInit, OnDestroy, ViewDidEnter {
+  map!: Leaflet.Map;
+
   constructor() { }
 
+  ngOnInit() { }
+  ionViewDidEnter() { this.leafletMap(); }
+
+  leafletMap() {
+    this.map = Leaflet.map('mapId').setView([-28.680272552112328, -49.374696627026985], 15);
+    Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'edupala.com © Angular LeafLet',
+    }).addTo(this.map);
+
+    Leaflet.marker([-28.680272552112328, -49.374696627026985]).addTo(this.map).bindPopup('Ponto de encontro').openPopup();
+    // Leaflet.marker([34, 77]).addTo(this.map).bindPopup('Leh').openPopup();
+
+  }
+
+  /** Remove map when we have multiple map object */
+  ngOnDestroy() {
+    this.map.remove();
+  }
 }
