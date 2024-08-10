0480f1f8311064b67edbeb39ff460c9e080b284b
Eduardo de Souza <desouzanovesete@gmail.com>
2024-08-09 23:03:06 -0300

    teste commit branch

--- src/app/eventos/lista-evento/lista-evento.component.ts
+++ src/app/eventos/lista-evento/lista-evento.component.ts
@@ -13,6 +13,13 @@ export class ListaEventoComponent implements OnInit, OnDestroy, ViewWillEnter {
   private subscriptions = new Subscription
 
   eventos: Evento[] = [];
+  public menus = [
+    { title: 'Solicitações pendentes', url: '/eventos', icon: 'person-add' },
+    { title: 'Configurações', url: '/eventos', icon: 'cog' },
+    { title: 'Usuários(Admin)', url: '/tabs/usuarios', icon: 'people' },
+    { title: 'Modalidades(Admin)', url: '/tabs/modalidades', icon: 'star' },
+    { title: 'Eventos(Admin)', url: '/tabs/eventos', icon: 'grid' },
+  ];
 
   constructor(private eventoService: EventoService,
     private alertController: AlertController,
