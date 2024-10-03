392af0e85fbd80e8da1b1b5a48abc26841ebb059
Eduardo de Souza <desouzanovesete@gmail.com>
2024-10-02 22:19:03 -0300

    ajuste teste app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,60 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  appPages = [
-    {
-      title: 'Schedule',
-      url: '/app/tabs/schedule',
-      icon: 'calendar'
-    },
-    {
-      title: 'Speakers',
-      url: '/app/tabs/speakers',
-      icon: 'people'
-    },
-    {
-      title: 'Map',
-      url: '/app/tabs/map',
-      icon: 'map'
-    },
-    {
-      title: 'About',
-      url: '/app/tabs/about',
-      icon: 'information-circle'
-    }
-  ];
-  loggedIn = false;
-  dark = false;
+  constructor(){}
 
-  constructor(
-  ) { }
-
-  initializeApp() {
-  }
-
-  checkLoginStatus() {
-  }
-
-  updateLoggedInStatus(loggedIn: boolean) {
-  }
-
-  listenForLoginEvents() {
-    window.addEventListener('user:login', () => {
-      this.updateLoggedInStatus(true);
-    });
-
-    window.addEventListener('user:signup', () => {
-      this.updateLoggedInStatus(true);
-    });
-
-    window.addEventListener('user:logout', () => {
-      this.updateLoggedInStatus(false);
-    });
-  }
-
-  logout() {
-  }
-
-  openTutorial() {
-  }
 }
